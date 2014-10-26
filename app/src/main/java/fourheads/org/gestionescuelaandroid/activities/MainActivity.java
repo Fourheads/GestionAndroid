c482e8a9d4ee0ed6b67a84de7fcfdcffdc2b66ef
Leandro Torroija <leanddro@hotmail.com>
2014-10-26 18:28:50 -0300

    creacion de los paquetes

--- app/src/main/java/fourheads/org/gestionescuelaandroid/activities/MainActivity.java
+++ app/src/main/java/fourheads/org/gestionescuelaandroid/activities/MainActivity.java
@@ -1,9 +1,14 @@
-package fourheads.org.gestionescuelaandroid;
+package fourheads.org.gestionescuelaandroid.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import fourheads.org.gestionescuelaandroid.R;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +17,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Captura del evento Click del boton
+        final Button button = (Button)findViewById(R.id.button_connect);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                button.setText(" x ");
+                //Intent intent = new Intent("android.intent.action.LIST");
+                //String[] valores = {"1","2","3","4","5","6","7","8","9","10"};
+                //intent.putExtra("valores", valores);
+                //startActivity(intent);
+            }
+        });
     }
 
 
